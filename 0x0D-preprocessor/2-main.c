b59a590dee142ae218cc834d44da764aaa9c668e
marcus0193 <marwanfaisal67@gmail.com>
2023-10-10 13:41:57 -0700

    last

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,11 +3,10 @@
 /**
  * main -  program that prints the name of the file it was compiled from
  *
- * Return: 0
+ * Return: nothing
 */
 
-int main()
+int main(void)
 {
 	printf("%s\n", __FILE__);
-	return (0);
 }
