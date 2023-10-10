e260fdd915f11bfaad0d7f79e6d5754c79608749
marcus0193 <marwanfaisal67@gmail.com>
2023-10-10 16:31:10 -0700

    first

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,12 @@
+/**
+ * struct dog - a new strcut of the name dog
+ * @age: the second member
+ * @*name: first member
+ * @*owner: the third member
+ *
+ * Description: a new struct for task 0 on 0x0E
+*/
+
 typedef struct dog{
 	char *name;
 	float age;
