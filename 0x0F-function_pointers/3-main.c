52c0b4f404e8b209cd139d9642213e1a9eb1bb73
marcus0193 <marwanfaisal67@gmail.com>
2023-10-11 19:44:03 -0700

    finaly

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,19 +11,33 @@
 */
 int main(int argc, char **argv)
 {
-	int (*k)(int, int), a, b;
+	int k, i, j;
 
 	if (argc != 4)
-		printf("Error\n"), exit(98);
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (((*argv[2] != '+') &&
+	(*argv[2] != '-') &&
+	(*argv[2] != '*') &&
+	(*argv[2] != '/') &&
+	(*argv[2] != '%')) ||
+	(strlen(argv[2]) != 1))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (*argv[3] == '0' && (*argv[2] == '/' || *argv[2] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
+	i = atoi(argv[1]);
+	j = atoi(argv[3]);
 
-	k = get_op_func(argv[2]);
-	if (!k)
-		printf("Error\n"), exit(99);
-	if (argv[3] = 0 && (argv[2] == '/') || (argv[2] == '%'))
-		printf("Error\n"), exit(100);
-	printf("%d\n", k(a, b));
+	k = (*get_op_func(argv[2]))(i, j);
+	printf("%d\n", k);
 	return (0);
 }
