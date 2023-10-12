ce0a74eea47d96c46f413409f82a72dbdb6903e5
marcus0193 <marwanfaisal67@gmail.com>
2023-10-12 03:28:44 +0300

    first

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,16 @@
+#ifndef HEDER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+
+
+#endif
