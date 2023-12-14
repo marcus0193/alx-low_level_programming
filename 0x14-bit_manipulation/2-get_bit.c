60da3ecf8011dcb378a7e0f514205fd4b2851bd6
marcus0193 <marwanfaisal67@gmail.com>
2023-12-15 02:07:43 +0300

    ll

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,8 +10,8 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	 if (index >= sizeof(n) * 8)
-		 return (-1);
+	if (index >= sizeof(n) * 8)
+		return (-1);
 
 	return (n >> index & 1);
 }
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * clear_bit - function that sets the value of a bit to 0 at a given index
+ * clear_bit - function that sets the value of a bit
+ *             to 0 at a given index
  *
  * @n: the index number
  * @index: the cleared bit
