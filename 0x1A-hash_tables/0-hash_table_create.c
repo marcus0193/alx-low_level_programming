0b6b6c50cd20a6eec7f1f151ea7d80a929325fae
marcus0193 <marwanfaisal67@gmail.com>
2024-02-15 18:07:57 -0800

    you better be a good boy mister checker

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,9 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash = malloc(sizeof(hash_table_t));
 	if (hash == NULL)
-	{
 		return (NULL);
-	}
 	hash->size = size;
 	hash->array = malloc(size * sizeof(hash_node_t *));
 	if (hash->array == NULL)
